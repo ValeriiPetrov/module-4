a9a6f916084df0f848a0a9356a60c22f59c3cd25
ValeriiPetrov <104438209+ValeriiPetrov@users.noreply.github.com>
2022-07-22 15:04:56 +0300

    module 4
    module 4

---
+++ SpeakGoodBye.js
@@ -0,0 +1,7 @@
+var byeSpeaker = {};
+(function () {   
+    var speakWord = "Good Bye";   
+    byeSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
+    } 
+})();
---
+++ SpeakHello.js
@@ -0,0 +1,7 @@
+var helloSpeaker = {};
+(function () { 
+    var speakWord = "Hello";    
+    helloSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
+    }   
+})();
